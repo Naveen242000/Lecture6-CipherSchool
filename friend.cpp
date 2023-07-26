7e9030abb6a546442852d068336aacd0bde5428b
Naveen242000 <111578221+Naveen242000@users.noreply.github.com>
2023-07-26 13:18:08 +0530

    friend.cpp

--- friend.cpp
+++ friend.cpp
@@ -1,1 +1,33 @@
-
+#include<bits/stdc++.h>
+using namespace std;
+class student{
+string passcode;
+friend class bestfriend;
+protected;
+int age;
+public:
+string name;
+int id;
+student(){
+}
+student(int id,string name, string passcode,int age)
+{
+this->name=name;
+this->id=id;
+this->passcode=passcode;
+this->age=age;
+}
+void into(){
+  cout<<"My name is"<<name<<" ,my id is"<<id<<"passcode is"<<passcode<<endl;
+}
+friend void hacker(student s);
+};
+void hacker(student s){
+  cout<<s.passcode<<" "<<s.age<<endl;
+}
+class bestfriend{
+public:
+void sharingSecret(student s){
+  cout<<s.passcode<<" "<<s.age<<endl;
+}
+};
